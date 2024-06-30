e48a85ba3ad268c9818106a9a8ce9f2326cce23c
Aman Joshi <139854820+CodingWorld-007@users.noreply.github.com>
2024-06-30 14:05:08 +0530

    Update Yt1.R

--- R programming/Yt1.R
+++ R programming/Yt1.R
@@ -1,1 +1,27 @@
+#Sum Operation
+x <- 1L 
+y <- 2  
+x <- x + y
+print(x)
+#typeof function
+print(typeof(x))
+
+#Difference Operation
+a <- 3L
+b <- 4L
+a <- a - b
+print(a)
+print(typeof(a))
+
+#Product Operation 
+a <- 3L
+b <- 4L
+a <- a * b
+print(a)
+print(typeof(a))
+
+#Reminder Operation
+r <- 10L %% 2L
+print(r)
+print(typeof(r))
 
